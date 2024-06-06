8535c2bd6d9b68b87290f37fd4d02251b5d0e6d3
Georgios Mhliwnhs <mhlionhsgeorgios@gmail.com>
2024-06-06 22:24:39 +0300

    #0005 Δηλωση του db context και του connection string για προετοιμασια για τα migration

--- peripatoiCrud.API/peripatoiCrud.API/Program.cs
+++ peripatoiCrud.API/peripatoiCrud.API/Program.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+using peripatoiCrud.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//εδω χρησιμοποιουμε dependency injection, περνοντας το dbcontext και υστερα παρεχουμε το connection string
+builder.Services.AddDbContext<PeripatoiDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("PeripatoiConnectionString")));
 
 var app = builder.Build();
 
