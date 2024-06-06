bca93c09a87e220f1af631725fb8362f27c4f78f
Georgios Mhliwnhs <mhlionhsgeorgios@gmail.com>
2024-06-06 22:17:46 +0300

    #0004 Δημιουργια dbContext και δηλωση των dbset

---
+++ peripatoiCrud.API/peripatoiCrud.API/Data/PeripatoiDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using peripatoiCrud.API.Models.Domain;
+
+namespace peripatoiCrud.API.Data
+{
+    public class PeripatoiDbContext : DbContext
+    {
+        public PeripatoiDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        //εδω οριζουμε τα db set μας βαση των μοντελων
+        public DbSet<Dyskolia> Dyskolies { get; set; }
+        public DbSet<Perioxh> Perioxes { get; set; }
+        public DbSet<Peripatos> Peripatoi { get; set; }
+    }
+}
